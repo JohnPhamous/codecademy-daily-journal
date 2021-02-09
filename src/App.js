fb3f732cc51e2013c58d9bf1423c197c0daea650
John Pham <john.pham@smartsheet.com>
2021-02-08 18:12:59 -0800

    Add error slice

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import BackgroundImage from "./features/backgroundImage/BackgroundImage";
 import Weather from "./features/weather/Weather";
 import Quote from "./features/quote/Quote";
+import Error from "./features/error/Error";
 
 function App() {
   return (
     <div className="App">
       <BackgroundImage />
+      <Error />
       <Weather />
       <Quote />
     </div>
--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import backgroundImageReducer from "../features/backgroundImage/backgroundImageS
 import journalReducer from "../features/journal/journalSlice";
 import quoteReducer from "../features/quote/quoteSlice";
 import weatherReducer from "../features/weather/weatherSlice";
+import errorReducer from "../features/error/errorSlice";
 
 export default configureStore({
   reducer: {
     backgroundImage: backgroundImageReducer,
     weather: weatherReducer,
     quote: quoteReducer,
-    journal: journalReducer
+    journal: journalReducer,
+    error: errorReducer
   }
 });
