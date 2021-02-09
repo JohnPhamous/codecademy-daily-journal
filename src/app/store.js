df09ff1ff8c6feaeea1de84834ce7f9f94bd8227
John Pham <john.pham@smartsheet.com>
2021-02-08 17:45:00 -0800

    Remove counterSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import backgroundImageReducer from "../features/backgroundImage/backgroundImageSlice";
-import counterReducer from "../features/counter/counterSlice";
 import journalReducer from "../features/journal/journalSlice";
 import quoteReducer from "../features/quote/quoteSlice";
 import weatherReducer from "../features/weather/weatherSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     backgroundImage: backgroundImageReducer,
     weather: weatherReducer,
     quote: quoteReducer,
