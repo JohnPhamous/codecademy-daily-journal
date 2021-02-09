29175dcd53ea7a724870f10577bb9079d36b62e2
John Pham <john.pham@smartsheet.com>
2021-02-08 17:56:12 -0800

    Add quotes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import BackgroundImage from "./features/backgroundImage/BackgroundImage";
 import Weather from "./features/weather/Weather";
+import Quote from "./features/quote/Quote";
 
 function App() {
   return (
     <div className="App">
       <BackgroundImage />
       <Weather />
+      <Quote />
     </div>
   );
 }
--- src/features/quote/quoteSlice.js
+++ src/features/quote/quoteSlice.js
@@ -1,11 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import quotesApi from "../../api/quotes";
+
+export const getQuote = createAsyncThunk("quote/getQuote", quotesApi.getQuote);
 
 export const quoteSlice = createSlice({
   name: "quote",
   initialState: {
-    quote: null
+    quote: "",
+    author: ""
   },
-  reducers: {}
+  reducers: {},
+  extraReducers: {
+    [getQuote.fulfilled]: (state, action) => {
+      state.quote = action.payload.quote;
+      state.author = action.payload.author;
+    }
+  }
 });
 
 export const {} = quoteSlice.actions;
