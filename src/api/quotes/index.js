44c4383017068b9f84b4285aeed3b54fd0041d1d
John Pham <johnphammail@gmail.com>
2021-02-09 16:24:51 -0800

    Use dot notation

--- src/api/quotes/index.js
+++ src/api/quotes/index.js
@@ -3,11 +3,11 @@ const API_URL = `https://quotes.rest/qod?language=en`;
 export const getQuote = async () => {
   const response = await fetch(API_URL);
   const json = await response.json();
-  const quote = json["contents"]["quotes"][0];
+  const quote = json.contents.quotes[0];
 
   return {
-    quote: quote["quote"],
-    author: quote["author"]
+    quote: quote.quote,
+    author: quote.author
   };
 };
 
